7e51ad1cdb6316168d0c02a72d0fe873bf910969
AkashMAbrrar <madborakash48@gmail.com>
2024-11-02 13:14:23 +0600

    this commit for navbar and a footer for fixed in this web page. now we going to create some components dynamicaly by outlets

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,7 +1,13 @@
+import { Outlet } from "react-router-dom";
+import Footer from "../Footer/Footer";
+import Navbar from "../Navbar/Navbar";
+
 const Root = () => {
   return (
     <div>
-      <h2>Hey Bro! Iam Here for Help You From Root.</h2>
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
